02b4af7c7d2e5bd2993e0c68b6a49e4febc1f109
zcbinhome <1191303744@qq.com>
2018-05-22 22:13:37 +0800

    堆排序

--- src/algorithm/HeapSort.java
+++ src/algorithm/HeapSort.java
@@ -32,4 +32,39 @@ public class HeapSort implements ISort {
             }
         }
     }
+
+    /******************华丽分割线 下面另一种写法的堆排序*********************/
+
+    private void heapify(int[] arr, int i, int size) {
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+        int max = i;
+        if (leftChild < size && arr[leftChild] > arr[max]) {
+            max = leftChild;
+        }
+        if (rightChild < size && arr[rightChild] > arr[max]) {
+            max = rightChild;
+        }
+        while (max != i) {
+            SortUtil.swap(arr, i, size);
+            heapify(arr, max, size);
+        }
+    }
+
+    private int buildHeap(int[] arr, int n) {
+        int heapSize = n;
+        for (int i = heapSize / 2 - 1; i >= 0; i--) {
+            heapify(arr, i, heapSize);
+        }
+        return heapSize;
+    }
+
+    public int[] heapSort(int arr[]) {
+        int heapSize = buildHeap(arr, arr.length - 1);
+        while (heapSize > 1) {
+            SortUtil.swap(arr, 0, --heapSize);
+            heapify(arr, 0, heapSize);
+        }
+        return arr;
+    }
 }
